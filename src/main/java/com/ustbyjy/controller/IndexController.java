38498665dd3f6e6d168f8b9de9a74322a3b394bd
yanjingyang@zulong.com <yanjingyang@zulong.com>
2017-11-27 20:32:53 +0800

    update

---
+++ src/main/java/com/ustbyjy/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.ustbyjy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2017/3/6
+ * Time: 17:22
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "html" + "/index.html";
+    }
+
+}
--- src/main/java/com/ustbyjy/service/DemoService.java
+++ src/main/java/com/ustbyjy/service/DemoService.java
@@ -1,17 +1,13 @@
 package com.ustbyjy.service;
 
-import com.ustbyjy.dao.DemoRepository;
-import com.ustbyjy.domain.Demo;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service
 public class DemoService {
-    @Resource
-    private DemoRepository demoRepository;
-
-    public void save(Demo demo) {
-        demoRepository.save(demo);
-    }
+//    @Resource
+//    private DemoRepository demoRepository;
+//
+//    public void save(Demo demo) {
+//        demoRepository.save(demo);
+//    }
 }
