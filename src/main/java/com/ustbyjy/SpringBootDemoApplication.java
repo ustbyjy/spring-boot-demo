0ecef32a0f54b8dce276f10c23d657968e7013ae
yanjingyang@zulong.com <yanjingyang@zulong.com>
2017-11-30 18:52:23 +0800

    文件上传

--- src/main/java/com/ustbyjy/SpringBootDemoApplication.java
+++ src/main/java/com/ustbyjy/SpringBootDemoApplication.java
@@ -6,9 +6,11 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.ustbyjy.interceptor.MyInterceptor1;
 import com.ustbyjy.interceptor.MyInterceptor2;
 import com.ustbyjy.servlet.MyServlet1;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
@@ -19,6 +21,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.MultipartConfigElement;
+
 /**
  * SpringBootApplication注解申明让Spring Boot自动给程序进行必要的设置，
  * 等价于以默认属性使用@Configuration、@EnableAutoConfiguration、@ComponentCan
@@ -27,10 +31,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableAsync
 @EnableCaching
 @ServletComponentScan
+// 使用@ComponentScan注解可以指定扫描的包，默认会扫描启动类同包以及子包下的注解
+//@ComponentScan(basePackages = "com.ustbyjy")
 public class SpringBootDemoApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        // 默认调用方法
+//        SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        // 自定义Banner显示方式，Banner.Mode.OFF:关闭，Banner.Mode.CONSOLE:控制台输出，默认方式，Banner.Mode.LOG:日志输出方式
+        SpringApplication springApplication = new SpringApplication(SpringBootDemoApplication.class);
+        springApplication.setBannerMode(Banner.Mode.CONSOLE);
+        springApplication.run(args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory multipartConfigFactory = new MultipartConfigFactory();
+        // 1KB、10MB
+        multipartConfigFactory.setMaxFileSize("100KB");
+        multipartConfigFactory.setMaxRequestSize("1MB");
+//        multipartConfigFactory.setLocation("doc");
+
+        return multipartConfigFactory.createMultipartConfig();
     }
 
     @Bean
--- src/main/java/com/ustbyjy/controller/FileUploadController.java
+++ src/main/java/com/ustbyjy/controller/FileUploadController.java
@@ -1,4 +1,75 @@
 package com.ustbyjy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.List;
+
+@Controller
 public class FileUploadController {
+
+    @RequestMapping("/file")
+    public String file() {
+        return "/file";
+    }
+
+    @RequestMapping("/multiFile")
+    public String multiFile() {
+        return "/multiFile";
+    }
+
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return "上传失败，" + e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败，" + e.getMessage();
+            }
+            return "上传成功";
+        } else {
+            return "上传失败，因为文件是空的.";
+        }
+    }
+
+    @RequestMapping(value = "/batch/upload", method = RequestMethod.POST)
+    public @ResponseBody
+    String handleFileUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        for (int i = 0; i < files.size(); ++i) {
+            file = files.get(i);
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    stream = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
+                    stream.write(bytes);
+                    stream.close();
+                } catch (Exception e) {
+                    stream = null;
+                    return "You failed to upload " + i + " => " + e.getMessage();
+                }
+            } else {
+                return "You failed to upload " + i + " because the file was empty.";
+            }
+        }
+        return "upload success";
+    }
 }
