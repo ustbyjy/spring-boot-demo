bb81081f766bfb28e03a20a1f12dbc64ce3899a7
ustbyjy <ustbyjjy@163.com>
2017-12-16 16:05:21 +0800

    spring session，设置session过期时间

--- src/main/java/com/ustbyjy/config/RedisSessionConfig.java
+++ src/main/java/com/ustbyjy/config/RedisSessionConfig.java
@@ -3,8 +3,11 @@ package com.ustbyjy.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+/**
+ * 默认过期时间1800s，也就是30分钟
+ */
 @Configuration
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisSessionConfig {
 
 }
