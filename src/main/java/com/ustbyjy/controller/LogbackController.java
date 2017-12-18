02a31873c57604a9ffc2cc32d0fe62d3235bd85e
yanjingyang@zulong.com <yanjingyang@zulong.com>
2017-12-18 20:31:30 +0800

    动态修改logback日志级别

--- src/main/java/com/ustbyjy/controller/LogbackController.java
+++ src/main/java/com/ustbyjy/controller/LogbackController.java
@@ -1,4 +1,31 @@
 package com.ustbyjy.controller;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LogbackController {
+
+    @RequestMapping(value = "/getLogLevel")
+    public Level getLogLevel(@RequestParam(value = "package") String packageName) throws Exception {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        return loggerContext.getLogger(packageName).getLevel();
+    }
+
+    @RequestMapping(value = "logLevel/{logLevel}")
+    public String logLevel(@PathVariable("logLevel") String logLevel,
+                           @RequestParam(value = "package") String packageName) {
+
+        LoggerContext loggerContext =(LoggerContext)LoggerFactory.getILoggerFactory();
+        loggerContext.getLogger(packageName).setLevel(Level.valueOf(logLevel));
+
+        return "ok";
+    }
 }
