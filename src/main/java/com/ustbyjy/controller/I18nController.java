ddf4e4da5aa034c3a22c22e2c05cf35d686d46b5
yanjingyang@zulong.com <yanjingyang@zulong.com>
2017-12-18 12:35:44 +0800

    国际化[2C

--- src/main/java/com/ustbyjy/controller/I18nController.java
+++ src/main/java/com/ustbyjy/controller/I18nController.java
@@ -1,4 +1,49 @@
 package com.ustbyjy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+@Controller
+@RequestMapping("/i18n")
 public class I18nController {
+
+    private static Logger logger = LoggerFactory.getLogger(I18nController.class);
+
+    @Autowired
+    private MessageSource messageSource;
+
+    /**
+     * 三种方式获取Locale：
+     * 1、直接写在参数列表中，Spring MVC自动装填
+     * 2、LocaleContextHolder.getLocale()；
+     * 3、RequestContextUtils.getLocale(request)；
+     *
+     * @param request
+     * @param locale
+     * @return
+     */
+    @RequestMapping(value = {"", "/"})
+    public String index(HttpServletRequest request, Locale locale) {
+        logger.error("locale1: " + locale);
+
+        Locale locale2 = LocaleContextHolder.getLocale();
+        logger.error("locale2: " + locale2);
+
+        Locale locale3 = RequestContextUtils.getLocale(request);
+        logger.error("locale3: " + locale3);
+
+        String msg = messageSource.getMessage("welcome", null, locale);
+        logger.error("msg: " + msg);
+
+        return "i18n";
+    }
 }
--- src/main/java/com/ustbyjy/util/LocaleMessageSourceUtil.java
+++ src/main/java/com/ustbyjy/util/LocaleMessageSourceUtil.java
@@ -1,4 +1,50 @@
 package com.ustbyjy.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
 public class LocaleMessageSourceUtil {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    /**
+     * @param code 对应messages配置的key
+     * @return String
+     */
+    public String getMessage(String code) {
+
+        return getMessage(code, null);
+    }
+
+
+    /**
+     * @param code 对应messages配置的key
+     * @param args 数组参数
+     * @return String
+     */
+
+    public String getMessage(String code, Object[] args) {
+        return getMessage(code, args, "");
+    }
+
+
+    /**
+     * @param code           对应messages配置的key
+     * @param args           数组参数
+     * @param defaultMessage 没有设置key时候的默认值
+     * @return
+     */
+
+    public String getMessage(String code, Object[] args, String defaultMessage) {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage(code, args, defaultMessage, locale);
+
+    }
 }
